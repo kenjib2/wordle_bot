2484f6c602efe4b243bcb28fc681238834ae032f
kbaugham <kbaugham@firstquadrant.com>
2022-02-07 15:03:35 -0800

    initial commit

---
+++ src/wordle_bot.py
@@ -0,0 +1,178 @@
+# ------------------- Weights -------------------
+WT_FIRST_WORD_LETTER_REPEAT = 0.50
+
+# -----------------------------------------------
+
+ALPHA = "abcdefghijklmnopqrstuvwxyz"
+
+
+def init_letter_freq_dict(file_path):
+    with open(file_path) as file:
+        lines = file.readlines()
+
+    letter_freqs = {}
+    for next_letter in ALPHA:
+        pos = [0, 0, 0, 0, 0]
+        for i in range(0, 4):
+            letter_freqs[next_letter] = pos
+
+    for next_line in lines:
+        for idx, next_letter in enumerate(next_line.strip().lower()):
+            letter_freqs[next_letter][idx] += 1
+
+    return letter_freqs
+
+
+def compute_word_score(word, letter_freqs, word_number):
+    score = 0
+    used_letters = set()
+    for idx, next_letter in enumerate(word):
+        letter_score = letter_freqs[next_letter][idx]
+        if next_letter in used_letters and word_number == 1:
+            letter_score *= WT_FIRST_WORD_LETTER_REPEAT
+        score += letter_score
+        used_letters.add(next_letter)
+    return score
+
+
+def init_word_set(file_path):
+    with open(file_path) as file:
+        word_set = file.readlines()
+    cleaned_word_set = [x.strip().lower() for x in word_set]
+    return [word for word in cleaned_word_set if len(word) == 5]
+
+
+def find_highest_scoring_word(letter_freqs, word_set, word_number):
+    highest_score = 0
+    highest_word = ""
+    for next_word in word_set:
+        score = compute_word_score(next_word, letter_freqs, word_number)
+        if score > highest_score:
+            highest_score = score
+            highest_word = next_word
+    return highest_word
+
+
+# Number of times the letter appears in yellow and green clues combined
+def num_clue_letters(letter, green_letters, yellow_letters):
+    num_letters = 0
+    for next_green_letter in green_letters:
+        if next_green_letter[0] == letter:
+            num_letters += 1
+    for next_yellow_letter in yellow_letters:
+        if next_yellow_letter[0] == letter:
+            num_letters += 1
+    return num_letters
+
+
+# Number of times the letter appears in the word
+def num_word_letters(word, letter):
+    num_letters = 0
+    for next_letter in word:
+        if next_letter == letter:
+            num_letters += 1
+    return num_letters
+
+
+def is_valid_word(word, green_letters, yellow_letters, gray_letters):
+    for next_letter in word:
+        word_letters = num_word_letters(word, next_letter)
+        clue_letters = num_clue_letters(next_letter, green_letters, yellow_letters)
+        if word_letters < clue_letters:
+            return False
+
+    for next_gray_letter in gray_letters:
+        word_letters = num_word_letters(word, next_gray_letter[0])
+        clue_letters = num_clue_letters(next_gray_letter[0], green_letters, yellow_letters)
+        if word_letters > clue_letters:
+            return False
+        if word_letters < clue_letters:
+            return False
+        if next_gray_letter[0] == word[next_gray_letter[1]]:
+            return False
+
+    green_positions = []
+    for next_green_letter in green_letters:
+        green_positions.append(next_green_letter[1])
+        if word[next_green_letter[1]] != next_green_letter[0]:
+            return False
+
+    # track which positions have already been "claimed" by a yellow
+    yellow_positions = []
+    for next_yellow_letter in yellow_letters:
+        # Can't be in the same positions or it would be green
+        if word[next_yellow_letter[1]] == next_yellow_letter[0]:
+            return False
+
+        letter_found = False
+        for idx, next_letter in enumerate(word):
+            if next_letter == next_yellow_letter[0] and idx not in yellow_positions and idx not in green_positions and not letter_found:
+                yellow_positions.append(idx)
+                letter_found = True
+
+        if not letter_found:
+            return False
+    return True
+
+
+def build_clue_sets(green_letters, yellow_letters, gray_letters, guessed_word, result):
+    # We can clear green and yellow clues because we are paring down the word set every iteration. That means all
+    # words we are checking already meet the criteria for previous clues, thus we don't need to worry about them
+    # anymore. This only works because we are always guessing only valid words that match all clues (aka hard mode).
+    # Do not clear grays, since those clues are still valid
+    green_letters.clear()
+    yellow_letters.clear()
+
+    for idx, next_letter in enumerate(result):
+        new_clue = (guessed_word[idx], idx)
+        if next_letter == 'G':
+            green_letters.add(new_clue)
+        elif next_letter == 'g':
+            gray_letters.add(new_clue)
+        if next_letter == 'y':
+            yellow_letters.add(new_clue)
+
+
+def pare_word_set(word_set, green_letters, yellow_letters, gray_letters):
+    pared_word_set = set()
+    for next_word in word_set:
+        if is_valid_word(next_word, green_letters, yellow_letters, gray_letters):
+            pared_word_set.add(next_word)
+    return pared_word_set
+
+
+def main():
+    green_letters = set()
+    yellow_letters = set()
+    gray_letters = set()
+
+    word_set = init_word_set("Collins Scrabble Words (2019).txt")
+    letter_freqs = init_letter_freq_dict("Wordles.txt")
+
+    num_guesses = 0
+
+    best_word = find_highest_scoring_word(letter_freqs, word_set, num_guesses + 1)
+
+    print("Best word choice: " + best_word)
+    finished = False
+    while not finished:
+        result = input("Results of last guess: ")
+        num_guesses += 1
+        if len(result) != 5 or not set(result).issubset("Ggy"):
+            print("Results must be five digits in the format Ggygg where g=gray, G=green, and y=yellow")
+        elif result == "GGGGG":
+            print("Winner is you!")
+            finished = True
+        elif num_guesses == 6:
+            print("You have been defeated!")
+            finished = True
+        else:
+            build_clue_sets(green_letters, yellow_letters, gray_letters, best_word, result)
+            word_set = pare_word_set(word_set, green_letters, yellow_letters, gray_letters)
+            best_word = find_highest_scoring_word(letter_freqs, word_set, num_guesses + 1)
+            print("Number of possible words: " + str(len(word_set)))
+            print("Best word choice: " + best_word)
+
+
+if __name__ == "__main__":
+    main()
